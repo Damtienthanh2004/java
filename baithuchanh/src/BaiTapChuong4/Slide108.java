6b8083c9836f7d19de6ee0dc9dafdc48d8c8bb75
Damtienthanh2004 <vexxart2004@gmail.com>
2024-05-19 21:47:27 +0700

    Bai thuc hanh chuong 4 ngay 15/5

--- baithuchanh/src/BaiTapChuong4/Slide108.java
+++ baithuchanh/src/BaiTapChuong4/Slide108.java
@@ -11,13 +11,13 @@ public class Slide108 {
         TreeMap.put(4, 10.1d);
         TreeMap.put(2, 7.2d);
         TreeMap.put(8, 20.28d);
-        System.out.println("Cac Phan tu co trong Treemap: ");
-        Set<Map.Entry<Integer, Double>> setTreeMap = TreeMap.entrySet();
-        System.out.println(setTreeMap);
+        System.out.println("Cac Phan tu co trong Treemap la: ");
+        Set<Map.Entry<Integer, Double>> SetTreeMap = TreeMap.entrySet();
+        System.out.println(SetTreeMap);
         TreeMap.replace(4, 20.11d);
         TreeMap.replace(2, 7.3d, 7.7d);
         System.out.println("Cac phan tu co trong TreeMap sau khi thay the: ");
-        setTreeMap = TreeMap.entrySet();
-        System.out.println(setTreeMap);
+        SetTreeMap = TreeMap.entrySet();
+        System.out.println(SetTreeMap);
     }
 }
